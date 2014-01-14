185eeec32816bbab5f5d3c024ab98a4bfa1a5580
futoase <futoase@gmail.com>
2014-01-15 02:17:37 +0900

    Modified: Support test-kitchen.

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,7 +9,7 @@ node.default['hrforecast']['application']['host'] = '0.0.0.0'
 
 # HRforecast install setting.
 node.default['hrforecast']['system']['user_name'] = 'hrforecast'
-node.default['hrforecast']['system']['perlbrew_ver'] = 'perl-5.18.0'
+node.default['hrforecast']['system']['perlbrew_ver'] = 'perl-5.18.2'
 node.default['hrforecast']['system']['install_dir'] = (
   "/home/#{node.default['hrforecast']['system']['user_name']}/hrforecast"
 )
