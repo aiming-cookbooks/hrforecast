8274303ab38d6fbcb53f0b85896fe6ec54d5b4c8
Keiji Matsuzaki <kmatsuzaki@aiming-inc.com>
2014-01-21 15:37:42 +0900

    Version up: v0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@aiming-inc.com'
 license          'All rights reserved'
 description      'Installs/Configures hrforecast'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 %w{yum git mysql perlbrew}.each do |depend|
   depends depend
